0db123733a5e49f6e62ed46f336a09a66bc05d63
yushijinhun <yushijinhun@gmail.com>
2017-12-30 23:59:58 +0800

    support travis-ci

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -xem
+cd server
+gradle clean bootJar
+cd ../test
+npm install .
+npm test
+../server/build/libs/yggdrasil-mock-server-*.jar &
+onexit(){
+	set +e
+	pkill -15 -P $(jobs -rp)
+	fg 1
+}
+trap onexit EXIT
+sleep 10 # wait for application to start
+if jobs -r|grep '\[1\]'>/dev/null;then
+	npm run integration-test
+else
+	exit 1
+fi
