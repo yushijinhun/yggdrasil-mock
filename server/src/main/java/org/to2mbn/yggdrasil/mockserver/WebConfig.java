b20e27cb278686735a3812401b6f288cdf94648c
yushijinhun <yushijinhun@gmail.com>
2018-04-05 11:07:38 +0800

    allow cors

---
+++ server/src/main/java/org/to2mbn/yggdrasil/mockserver/WebConfig.java
@@ -0,0 +1,18 @@
+package org.to2mbn.yggdrasil.mockserver;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class WebConfig implements WebFluxConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedMethods("*")
+				.allowCredentials(false);
+	}
+}
